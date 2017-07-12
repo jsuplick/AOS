13f59f0b325902cdc8dabf45ad4873d5c2534e22
Jean Suplick <jean.suplick@orasi.com>
2017-07-11 22:41:52 -0400

    defect #48013: Expanded the methods

--- order/src/main/java/com/advantage/order/store/dao/AbstractRepository.java
+++ order/src/main/java/com/advantage/order/store/dao/AbstractRepository.java
@@ -10,3 +10,8 @@ public abstract class AbstractRepository {
     @PersistenceContext
     protected EntityManager entityManager;
 }
+
+public abstract class ConcreteRepository {
+    @PersistenceContext
+    protected EntityManager entityManager;
+}
